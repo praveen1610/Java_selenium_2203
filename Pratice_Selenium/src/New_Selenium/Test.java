e3a44ee4f472a50c7046de00919d3691008d4302
unknown <praveenkumar.jaya@PJAYABALAN1-LT.tul.solarwinds.net>
2016-03-22 18:38:16 +0530

    Selenium lib location modified

--- Pratice_Selenium/src/New_Selenium/Test.java
+++ Pratice_Selenium/src/New_Selenium/Test.java
@@ -3,6 +3,7 @@ package New_Selenium;
 public class Test {
 public static void main(String[] args) {
 		
-		webDriver driver = new FirefoxDriver();
+		//webDriver driver = new FirefoxDriver();
 		  
 	}
+}
