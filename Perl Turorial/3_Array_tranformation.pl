9182ea60a8b5dd1dcf9154ba4cb2dae13dde88bd
praveen1610 <praveen_world99@yahoo.com>
2016-03-25 15:19:21 +0530

    Perl- 6_condition_statement, 3_array_tranformation

---
+++ Perl Turorial/3_Array_tranformation.pl
@@ -0,0 +1,20 @@
+#transforming string to array
+$inputString = "hello-world-never-ends-easiy";
+$inputstring_2 = "praveen,kumar,prema,vetri,vel,jayabalan";
+
+@array = split('-',$inputString);
+@array_2 = split(',',$inputstring_2);
+@array_2 = sort(@array_2);
+$size = @array;
+for($i=0; $i<$size; $i++)
+{
+print "$array[$i]\n";
+print "$array_2[$i]\n";    
+}
+
+#transforming array to string
+$convertedString_2 = join('-',@array_2);
+print "$convertedString_2\n";
+
+$convertedString = join(',',@array);
+print "$convertedString\n";
---
+++ Perl Turorial/6_condition_statement.pl
@@ -0,0 +1,16 @@
+#if else statement
+$a = 12;
+if($a<10){
+    print "inside if condition\n";
+}
+elsif($a<14)
+{
+    print "inside elsif condition\n";
+}
+else{
+    
+}
+unless($a<10)
+{
+    print "inside unless condition\n";
+}
