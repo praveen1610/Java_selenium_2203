d58796aae24138d9c23b9aec35cc459239fe3d76
unknown <praveenkumar.jaya@PJAYABALAN1-LT.tul.solarwinds.net>
2016-04-05 19:22:58 +0530

    perl-File Handler and directories

--- Perl Turorial/g_fileHandler.pl
+++ Perl Turorial/g_fileHandler.pl
@@ -1,10 +1,33 @@
 #!usr/bin/perl
+#require to create a text file with name console.txt in location c:\selenium-2.50.0\\selenium-2.53.0\
 
-$filelocation = "C:\\Users\\User\\workspace\\TestNG\\Notepad_reading_input";
-open(DATAS, $filelocation) or die "open file failed,$!";
+$filelocation = "C:\\selenium-2.50.0\\selenium-2.53.0\\console.txt";
+open(DATAS, ">>$filelocation") or die "open file failed,$!";  #appending data to file
+print DATAS "a) first testing\n";
+print DATAS "b) Second testing\n"; 
+close DATAS;
 
-while(<DATAS>)
-{
-    print "$_";
+open(DATAS_1, "<$filelocation") or die "open file failed,$!";
+@lines = <DATAS_1>;   #reading and converting file to array
+print $lines[2];
+close DATAS_1;
+
+open(DATAS_3, "<$filelocation") or die "open file failed,$!";
+#while(<DATAS_3>)        #reading and printing the file line by line
+#{
+    #print "$_";
+#}
+open(DATAS_4, ">C:\\selenium-2.50.0\\selenium-2.53.0\\console_copy.txt") or die "open file failed,$!";   #appending data to file
+while(<DATAS_3>)
+{ 
+    print DATAS_4 $_;
+    
 }
+close (DATAS_3);
+close (DATAS_4);
 
+#Deleting a file
+unlink("$filelocation");
+
+#renaming the file name
+rename("C:\\selenium-2.50.0\\selenium-2.53.0\\console_copy.txt", "C:\\selenium-2.50.0\\selenium-2.53.0\\console_new.txt");
---
+++ Perl Turorial/h_directories.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+# Display all the files in /tmp directory.
+use package T;
+
+$dir = "c:/*.sys";    #will display all files with extension .sys
+my @files = glob( $dir );
+foreach (@files ){
+    print $_ . "\n";
+}
+
+#list all the folder and file in a location
+opendir(DIR,'c:/') or die "could not open the directory -$!";
+while ($file = readdir DIR){
+    print "$file\n"
+}
+closedir DIR;
+
+
+$newdir = "C:/testerdirec";
+mkdir($newdir);   #create new file use rmdir($newdir) to remove the directory
+
+chdir($newdir);
+open(newfile,">newfile");
+print newfile "testing";
+close newfile;
