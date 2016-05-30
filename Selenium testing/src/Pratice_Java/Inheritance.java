e924a75211bc9b6ccf1750012e8aedfb6de25691
praveen1610 <praveen_world99@yahoo.com>
2016-05-30 08:09:03 +0530

    no message

--- Selenium testing/src/Pratice_Java/Inheritance.java
+++ Selenium testing/src/Pratice_Java/Inheritance.java
@@ -1,28 +1,24 @@
 package Pratice_Java;
 
-
 class a {
-
-	public void add(){
-		int a = 10, b = 20;
-		int c = a + b;
+	int c;
+	public void add(int a, int b){
+		c = a + b;
 		System.out.println("Value of c is " +c);
 	}
 }
+
 class b extends a {
 	public void sub(){
-		int a = 10, b = 20;
-		int d = b -a;
-		System.out.println("value of d is "+d);
+		int a = 10; int b = 6;
+		c = b -a;
+		System.out.println("value of d is "+c);
 	}
 }
 public class Inheritance {
 	public static void main (String args[]){	
-		a objects = new b();
-		objects.add();
-		
-		
 		b objects1 = new b();
+		objects1.add(5,6);
 		objects1.sub();
 		
 	}
