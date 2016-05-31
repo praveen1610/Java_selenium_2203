d08b8bcea96012c3e6862ebb2e9207346d015fdc
unknown <praveenkumar.jaya@PJAYABALAN1-LT.tul.solarwinds.net>
2016-05-31 17:34:25 +0530

    no message

--- cashkaro/src/com/cashkaro/pages/Login_Page.java
+++ cashkaro/src/com/cashkaro/pages/Login_Page.java
@@ -1,9 +1,24 @@
 package com.cashkaro.pages;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class Login_Page {
 	By signin_popup = By.cssSelector(".fl.signin_popupbox");
-	//By singup_framework = 
+	
+	WebDriver driver;
+
+		
+	public Login_Page(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public void Click_siginButton(){
+		driver.findElement(signin_popup).click(); 
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+	
 
 }
--- cashkaro/src/com/cashkaro/pages/sign_up_page.java
+++ cashkaro/src/com/cashkaro/pages/sign_up_page.java
@@ -1,5 +1,7 @@
 package com.cashkaro.pages;
 
+import java.io.FileInputStream;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -8,11 +10,20 @@ import org.openqa.selenium.WebDriver;
 public class sign_up_page {
 	
 	WebDriver driver;
+	/*
+	Properties prop = new Properties();
+	FileInputStream file = new FileInputStream("Xpathstorage.properties");
+	prop.load(Xpathstorage.properties); */
+	
 	By facebookemail_login	 = By.xpath("//input[@id='email']");
 	By facebook_password	 = By.xpath("//input[@id='pass']");
     By facebook_loginButton  = By.xpath("//input[@value='Log In']");
     By facebook_JoinWith 	 = By.xpath("//a[@id='close_and_go_fb']");
    
+	/*By facebookemail_login	 = By.xpath(facebookemail_login);
+	By facebook_password	 = By.xpath(facebook_password);
+    By facebook_loginButton  = By.xpath(facebook_loginButton);
+    By facebook_JoinWith 	 = By.xpath(facebook_JoinWith);*/
 
 	public sign_up_page(WebDriver driver){
 		this.driver = driver;
@@ -27,10 +38,16 @@ public class sign_up_page {
 		driver.findElement(facebook_loginButton).click();
 	}
 	public void ClickJoinWithFBButton(){
+		String mainHandle= driver.getWindowHandle();
+		
 		driver.findElement(facebook_JoinWith).click();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		 for (String winHandle : driver.getWindowHandles()) {
-		      driver.switchTo().window(winHandle);
+		
+		for (String winHandle : driver.getWindowHandles()) {
+			 driver.switchTo().window(winHandle);
+		     if (driver.getTitle().equals("Facebook")){
+		    	 break; 
+			 }
 		  }
 	}
 
