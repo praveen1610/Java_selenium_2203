18e05078357ca2cd3b1de415864bd85892295038
unknown <praveenkumar.jaya@PJAYABALAN1-LT.tul.solarwinds.net>
2016-06-05 23:57:54 +0530

    Xpath updated

--- CashkaroProject/src/Helper/Locator_repositoryReader.java
+++ CashkaroProject/src/Helper/Locator_repositoryReader.java
@@ -19,12 +19,12 @@ public class Locator_repositoryReader {
 	
 	XPath xpathtest;
 	Document Doc;
-	public static void main(String[] args){
-		Locator_repositoryReader test = new Locator_repositoryReader("XML_input.xml");
-		String test1 = test.getlocation("SubmitButton");
-		System.out.println(test1);
-		
-	}
+//	public static void main(String[] args){
+//		Locator_repositoryReader test = new Locator_repositoryReader("XML_input.xml");
+//		String test1 = test.getlocation("SubmitButton");
+//		System.out.println(test1);
+//		
+//	}
 	public Locator_repositoryReader (String LocationFileName){
 		try{
 			
@@ -48,13 +48,13 @@ public class Locator_repositoryReader {
 		NodeList nodeList;
 		String attributetest = null;
 		try {
-			nodeList = (NodeList) xpathtest.compile("/Xpath").evaluate(Doc, XPathConstants.NODESET);
+			nodeList = (NodeList) xpathtest.compile("/Locator").evaluate(Doc, XPathConstants.NODESET);
 			Node nNode = nodeList.item(0);
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
 				Element elementtest = (Element) nNode;
 				attributetest = elementtest.getElementsByTagName(TagName).item(0).getTextContent();
 			}
-		} 
+			}
 		catch (XPathExpressionException e) {
 			e.printStackTrace();
 		}
